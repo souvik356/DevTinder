bcdddfd57524aca8d2e4bda05020fb2f9af5a711
Souvik Roy <souvikroy@Souviks-MacBook-Air.local>
2024-11-13 01:15:37 +0530

    understanding routing and HTTP protocol

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ const express = require('express')
 
 const app = express()
 
-app.use('/',(req,res)=>{
-   res.send('Namaste Souvik')
+// app.use('/user',(req,res)=>{
+//    res.send('HAHAHAH')
+// })
+
+app.get('/user',(req,res)=>{
+    console.log(req.query);
+   res.send({firstName: 'Souvik',lastName: 'Roy'})
+})
+
+// app.get('/user/:userId/:password',(req,res)=>{
+    // console.log(req.query);
+//     console.log(req.params);
+    
+//    res.send({first_name:'Souvik',last_name:'Roy'})
+// })
+
+// app.get(/.*fly/,(req,res)=>{
+//     res.send({firstName: 'Souvik',lastName: 'Roy'})
+//   })
+
+app.post('/user',(req,res)=>{
+   res.send('Data posted succesfully')
+})
+
+app.delete('/user',(req,res)=>{
+    res.send('Data deleted succesfully')
 })
 
 app.use('/test',(req,res)=>{
-   res.send('Test server is running')
+    res.send('Test 2 server is running')
 })
 
-app.use('/dashboard',(req,res)=>{
-   res.send('Dasboard server is running')
-})
-
-app.use((req,res)=>{
-  res.send('Hello from server')
-})
 
 app.listen(3000,()=>{
     console.log('Server is succesfully lisetning on port 3000');
